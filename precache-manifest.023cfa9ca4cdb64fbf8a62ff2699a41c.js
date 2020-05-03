08e141add0ccab79f27a4a34d005c5efd1a8489c
ongian <ongchristian117@gmail.com>
2020-05-04 06:08:05 +0800

    Add files via upload

---
+++ precache-manifest.023cfa9ca4cdb64fbf8a62ff2699a41c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2712454e727b2224e465db4fe9aefee9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c4096b0fcbaee3f91bb",
+    "url": "/static/css/main.315c8994.chunk.css"
+  },
+  {
+    "revision": "5357bbf6d8f5f4988461",
+    "url": "/static/js/2.c0c0d5ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c0c0d5ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c4096b0fcbaee3f91bb",
+    "url": "/static/js/main.dacffb33.chunk.js"
+  },
+  {
+    "revision": "fc795b479c28d9d3d1af",
+    "url": "/static/js/runtime-main.4224af9b.js"
+  }
+]);
